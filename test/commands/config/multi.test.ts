086752c79c4ce55c2cb9f77efbc25f5209836d14
Travis <tlongoria@salesforce.com>
2018-06-20 22:46:31 -0600

    Clean-up tslint issues - created import class to get around no-duplicate-imports issue - performed various clean-up for tslint

--- test/commands/config/multi.test.ts
+++ test/commands/config/multi.test.ts
@@ -1,20 +1,16 @@
-import Nock from '@fancy-test/nock'
-import {expect} from '@oclif/test'
-import * as Test from '@oclif/test'
-export const test = Test.test.register('nock', Nock);
+import {expect, test} from '../../test'
 
 describe('config:multi', () => {
-
   test
       .nock('https://api.heroku.com', api => api
         .get('/apps/mysterious-reef/config-vars')
-        .reply(200, {"NPM_CONFIG_PRODUCTION": "false"})
+        .reply(200, {NPM_CONFIG_PRODUCTION: false})
         .get('/apps/giggly-gorilla/config-vars')
-        .reply(200, {"RACK_ENV": "production"})
+        .reply(200, {RACK_ENV: 'production'})
       )
       .stdout()
       .command(['config:multi', 'mysterious-reef,giggly-gorilla'])
-      .it('should display config vars',  ctx => {
+      .it('should display config vars', ctx => {
         expect(ctx.stdout).to.contain('NPM_CONFIG_PRODUCTION=false')
         expect(ctx.stdout).to.contain('RACK_ENV=production')
       })
@@ -22,11 +18,11 @@ describe('config:multi', () => {
   test
     .nock('https://api.heroku.com', api => api
       .get('/apps/unknown-unicorn/config-vars')
-      .reply(404, {"NPM_CONFIG_PRODUCTION": "false"})
+      .reply(404, {NPM_CONFIG_PRODUCTION: false})
     )
     .stdout()
     .command(['config:multi', 'unknown-unicorn'])
-    .it('should inform user if app is not found',  ctx => {
+    .it('should inform user if app is not found', ctx => {
       expect(ctx.stdout).to.contain('Couldn\'t find application: \'unknown-unicorn\'')
     })
 })
